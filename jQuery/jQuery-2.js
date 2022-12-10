0a0866a9cbe048a82263852a4b19dd8e45ba65b7
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-12-10 19:33:08 +0100

    jquery

--- jQuery/jQuery-2.js
+++ jQuery/jQuery-2.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  
+  // Target multiple items and set mutliple css changes on them.
+  $(".p1, .p2").css({
+    color: "blue",
+    paddingLeft: "10px",
+    fontSize: "20px",
+    background: "lightgray",
+    borderRadius: "12px",
+    marginTop: "20px",
+  });
+
+  $("h3").css("textAlign", "center");
+  $("button").css("margin", "auto");
 });
